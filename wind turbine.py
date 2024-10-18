142d91d54d2d0fed788ccf4092488eaf0f8f5977
Joost Jaspers <joostjaspers02@gmail.com>
2024-10-18 17:28:09 +0200

    Tried to implement smaller windturbine

--- wind turbine.py
+++ wind turbine.py
@@ -7,15 +7,36 @@ import numpy as np
 def calculatewind():
     """Function that returns the average wind turbine output using windpowerlib"""
     
-    # Define the wind turbine based on a standard turbine model in the library
-    turbine_data = {
-        'turbine_type': 'E-126/4200',  # Example turbine (Enercon E-126, 4.2MW rated power)
-        'hub_height': 135  # Hub height in meters
+    #Define a custom power curve for the Bergey Excel 1
+    power_curve_data = {
+    0: 0,    # Wind speed 0 m/s -> Power output 0 kW
+    2.5: 0,  # Wind speed 2.5 m/s -> Power output 0 kW (cut-in speed)
+    5: 0.25, # Wind speed 5 m/s -> Power output 0.25 kW
+    7: 0.5,  # Wind speed 7 m/s -> Power output 0.5 kW
+    9: 0.75, # Wind speed 9 m/s -> Power output 0.75 kW
+    11: 1,   # Wind speed 11 m/s -> Power output 1 kW (rated speed)
+    12: 1,   # Wind speed 12 m/s -> Power output 1 kW
+    15: 0.8, # Wind speed 15 m/s -> Power output 0.8 kW
+    20: 0,   # Wind speed 20 m/s -> Power output 0 kW (cut-out speed)
     }
-    
+
+    turbine_data = {
+    'turbine_type': 'Bergey Excel 1',
+    'hub_height': 20,               # Hub height in meters
+    'rotor_diameter': 2.5,          # Rotor diameter in meters (approx.)
+    'rated_power': 1.0,             # Rated power in kW
+    'cut_in_wind_speed': 2.5,       # Cut-in wind speed in m/s
+    'rated_wind_speed': 11.0,       # Rated wind speed in m/s
+    'cut_out_wind_speed': 20.0,     # Cut-out wind speed in m/s
+    'power_curve': pd.DataFrame(
+            data={'value': [p * 1000 for p in [
+                      0, 0, 250, 500, 750, 1000, 1000, 800, 0]],  # in W
+                  'wind_speed': [0.0, 2,5, 5.0, 7, 9, 11, 12, 15, 20]})  # in m/s
+    }
+   
     # Instantiate a WindTurbine object
     turbine = WindTurbine(**turbine_data)
-    
+
     # Average wind speed in Perth (miles/hour) (example data)
     average_wind_speed_miles_hour = np.array([14.0, 13.7, 12.9, 11.8, 11.8, 12.6,
                           12.5, 12.1, 12.2, 12.5, 13.4, 13.9])
